c89ae024a86f45d8104c6eff00a654fd4b34a7d0
Eduardo Barrios <guayoswing@gmail.com>
2017-11-08 22:36:24 -0600

    Busqueda de viajes

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -3,14 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using PagedList;
+using Web.Models;
+using Web.Models.ViewModels;
 
 namespace Web.Controllers
 {
    
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
-        {
+        {                        
             return View();
         }
 
@@ -27,11 +34,40 @@ namespace Web.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(int? page)
         {
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            var lista = db.Viajes.ToList();
+            int pageSize = 2;
+            int pageNumber = (page ?? 1);
+            return View(lista.ToPagedList(pageNumber,pageSize));
+        }
+
+        
+
+        [HttpPost]
+        public ActionResult BuscarViajes(BuscarViaje model, int? page)
+        {
+            
+
+            //Obteniendo el listado de la tabla AspNetUsers
+            var ctx = Request.GetOwinContext().Get<ApplicationDbContext>();
+            //var users = ctx.Users;
+            var manager = new ApplicationUserManager(new UserStore<ApplicationUser>(ctx));
+            var users = manager.Users.ToList();
+
+            
+
+            BuscarViaje viewModel = new BuscarViaje();
+            List<Viaje> listaViajes = viewModel.listaViajesBuscados(model);
+
+            int pageSize = 2;
+            int pageNumber = (page ?? 1);
+
+            //PagedList<Viaje> modelPaginado = new PagedList<Viaje>(listaViajes, pageNumber, pageSize);
+            
+            return View("BusquedaViajes", listaViajes/*.ToPagedList(pageNumber,pageSize)*/);
         }
     }
 }
---
+++ Web/Models/ViewModels/BuscarViaje.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace Web.Models.ViewModels
+{
+    public class BuscarViaje
+    {
+        private ApplicationDbContext db;
+
+        public BuscarViaje()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        [Display(Name = "Punto de Partida")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string PuntoPartida { get; set; }
+
+        [Display(Name = "Punto de Destino")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string PuntoDestino { get; set; }
+
+        [DataType(DataType.Date)]
+        public string Fecha { get; set; }
+
+        public List<Viaje> listaViajesBuscados(BuscarViaje model)
+        {
+            var context = new IdentityDbContext();
+            var allUsers = context.Users.ToList();
+            //foreach( var item in allUsers)
+            //{
+            //    Console.WriteLine(item.Id+" "+item.UserName);
+            //}
+
+            //var viajes = (from v in db.Viajes                          
+            //              where (v.PuntoPartida.Contains(model.PuntoPartida))
+            //                    ||
+            //                    (v.PuntoPartida.Contains(model.PuntoDestino))                          
+            //              select v).ToList();
+
+            //var viajes = (from Viajes in db.Viajes
+            //              join Usuarios in allUsers
+            //              on Viajes.ApplicationUserId equals Usuarios.Id                                                   
+            //              select Viajes).ToList();
+
+            var viajes = db.Viajes
+                        .Include("ApplicationUser")
+                        .Where(v => v.PuntoPartida.Contains(model.PuntoPartida))
+                        .Where(v => v.PuntoDestino.Contains(model.PuntoDestino))
+                        .ToList();
+
+           foreach(var item in viajes)
+            {
+                Console.WriteLine(item.ApplicationUser.Id+" "+item.ApplicationUser.UserName+" "+item.PuntoPartida);
+            }
+
+            return viajes;
+        }
+    }
+}
